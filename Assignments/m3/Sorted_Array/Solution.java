b87c1a24b56095e397cf9f71d97aaee9683b45e7
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-09-26 22:12:47 -0700

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- Assignments/m3/Sorted_Array/Solution.java
+++ Assignments/m3/Sorted_Array/Solution.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
  * Class for sorted array.
  */
 class SortedArray {
-	/**
-	 * size1.
-	 */
-	private int size1;
-	/**
-	 * size2.
-	 */
+    /**
+     * size1.
+     */
+    private int size1;
+    /**
+     * size2.
+     */
     private int size2;
     /**
      * array1.
@@ -30,40 +30,40 @@ class SortedArray {
      * @param      a     { parameter_description }
      * @param      b     { parameter_description }
      */
-	public SortedArray (final int[] a, final int[] b) {
-		this.size1 = a.length;
-		this.size2 = b.length;
-		this.array1 = a;
-		this.array2 = b;
-		this.array3 = new int[size1 + size2];
-	}
-	/**
-	 * sortArray.
-	 *
-	 * @param      a1    A 1
-	 * @param      b1    The b 1
-	 *
-	 * @return     returns sorted array.
-	 */
-	public int[] sortArray(final int[] a1, final int[] b1) {
-		int i = 0;
-		int j = 0;
-		int k = 0;
-		while (i < size1 && j < size2) {
-			if (a1[i] < b1[j]) {
-				array3[k++] = a1[i++];
-			} else {
-				array3[k++] = b1[j++];
-			}
-		}
-		while (i < size1) {
-			array3[k++] = a1[i++];
-		}
-		while (j < size2) {
-			array3[k++] = b1[j++];
-		}
-		return array3;
-	}
+    SortedArray (final int[] a, final int[] b) {
+        this.size1 = a.length;
+        this.size2 = b.length;
+        this.array1 = a;
+        this.array2 = b;
+        this.array3 = new int[size1 + size2];
+    }
+    /**
+     * sortArray.
+     *
+     * @param      a1    A 1
+     * @param      b1    The b 1
+     *
+     * @return     returns sorted array.
+     */
+    public int[] sortArray(final int[] a1, final int[] b1) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        while (i < size1 && j < size2) {
+            if (a1[i] < b1[j]) {
+                array3[k++] = a1[i++];
+            } else {
+                array3[k++] = b1[j++];
+            }
+        }
+        while (i < size1) {
+            array3[k++] = a1[i++];
+        }
+        while (j < size2) {
+            array3[k++] = b1[j++];
+        }
+        return array3;
+    }
 }
 
 /**
@@ -88,20 +88,21 @@ public final class Solution {
         int[] arr1 = new int[n];
         int[] arr2 = new int[m];
         if (n > 0) {
-        	String[] tokens = input.next().split(",");
-        	for (int i = 0; i < n; i++) {
-        		arr1[i] = Integer.parseInt(tokens[i]);
-        	}
+            String[] tokens = input.next().split(",");
+            for (int i = 0; i < n; i++) {
+                arr1[i] = Integer.parseInt(tokens[i]);
+            }
         }
         if (m > 0) {
-        	String[] token = input.next().split(",");
-        	for (int i = 0; i < m ; i++) {
-        		arr2[i] = Integer.parseInt(token[i]);
-        	}
+            String[] token = input.next().split(",");
+            for (int i = 0; i < m; i++) {
+                arr2[i] = Integer.parseInt(token[i]);
+            }
         }
         SortedArray s = new SortedArray(arr1, arr2);
         int[] result = s.sortArray(arr1, arr2);
-        System.out.println(Arrays.toString(result).substring(1, Arrays.toString(result).length() - 1).replace(" ", ""));
+        System.out.println(Arrays.toString(result).substring(
+            1, Arrays.toString(result).length() - 1).replace(" ", ""));
     }
 }
 
