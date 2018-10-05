e325f2b03bbab982d3f5072276263e315a4900aa
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-04 19:37:57 -0700

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
+++ Assignments/m10/10.1 LinkedList using Recursion/LinkedList.java
@@ -1,26 +1,72 @@
 import java.util.Scanner;
+/**
+ * Class for node.
+ */
 class Node {
+	/**
+	 * data.
+	 */
 	int data;
+	/**
+	 * node object next.
+	 */
 	Node next;
-	Node(int val) {
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      val   The value
+	 */
+	Node(final int val) {
 		this.data = val;
 	}
 }
+/**
+ * List of linkeds.
+ */
 class LinkedList {
+	/**
+	 * { var_description }
+	 */
 	Node head;
-	int size = 0;
+	/**
+	 * { var_description }
+	 */
+	int size;
+	/**
+	 * Constructs the object.
+	 */
 	LinkedList() {
 		head = null;
 		size = 0;
 	}
-	void insertAt(int position, int val)throws Exception {
+	/**
+	 * { function_description }
+	 *
+	 * @param      position   The position
+	 * @param      val        The value
+	 *
+	 * @throws     Exception  { exception_description }
+	 */
+	void insertAt(final int position, final int val)throws Exception {
 		if (position < 0 || position > size) {
 			throw new Exception();
 		}
 		Node newNode = new Node(val);
 		head = insertAt(position, head, newNode, 0);
 	}
-	Node insertAt(int position, Node head, Node newNode, int count) throws Exception {
+	/**
+	 * { function_description }
+	 *
+	 * @param      position   The position
+	 * @param      head       The head
+	 * @param      newNode    The new node
+	 * @param      count      The count
+	 *
+	 * @return     { description_of_the_return_value }
+	 *
+	 * @throws     Exception  { exception_description }
+	 */
+	Node insertAt(final int position, final Node head, final Node newNode, final int count) throws Exception {
 		if (position == count) {
 			newNode.next = head;
 			size++;
@@ -29,10 +75,19 @@ class LinkedList {
 		head.next = insertAt(position, head.next, newNode, count+1);
 		return head;
 	}
+	/**
+	 * { function_description }
+	 */
 	void reverse() {
 		reverse(null, head);
 	}
-	void reverse(Node preTemp, Node temp) {
+	/**
+	 * { function_description }
+	 *
+	 * @param      preTemp  The pre temporary
+	 * @param      temp     The temporary
+	 */
+	void reverse(final Node preTemp, final Node temp) {
 		if (temp != null) {
 			reverse(temp, temp.next);
 			temp.next = preTemp;
@@ -40,6 +95,9 @@ class LinkedList {
 			head = preTemp;
 		}
 	}
+	/**
+	 * { function_description }
+	 */
 	void display() {
 		Node temp = head;
 		String str = "";
