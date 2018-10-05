a913b182d60d4c0172b022853e271b6722dd1447
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-04 19:31:56 -0700

    "ads1_m10_a1: 3/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m10/10.1 LinkedList using Recursion/Solution.java
+++ Assignments/m10/10.1 LinkedList using Recursion/Solution.java
@@ -24,8 +24,7 @@ public final class Solution {
                 try {
                 list.insertAt(Integer.parseInt(tokens[1]),
                 Integer.parseInt(tokens[2]));
-                list.display();
-            }
+                list.display(); }
             catch (Exception e) {
                 System.out.println("Can't insert at this position.");
             }
@@ -33,8 +32,7 @@ public final class Solution {
             case "reverse":
             try {
             list.reverse();
-            list.display();
-        }
+            list.display(); }
             catch (Exception e) {
                 System.out.println("No elements to reverse.");
             }
