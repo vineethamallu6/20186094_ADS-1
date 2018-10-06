a09e40be511eaa426288f639dc8dfa678cb2578f
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-06 13:17:15 -0700

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m12/Assignment-1/Solution.java
+++ Assignments/m12/Assignment-1/Solution.java
@@ -1,135 +1,325 @@
 import java.util.Scanner;
-class Student{
-	private String name;
-	private String date;
-	private int m1;
-	private int m2;
-	private int m3;
-	private int total;
-	private String caste;
-	Student(String n, String d, int m1, int m2, int m3, int t, String c) {
-		this.name = n;
-		this.date = d;
-		this.m1 = m1;
-		this.m2 = m2;
-		this.m3 = m3;
-		this.total = t;
-		this.caste = c;
-	}
-	public String getName() {
-		return this.name;
-	}
-	public String getDate() {
-		return this.date;
-	}
-	public int getM1() {
-		return this.m1;
-	}
-	public int getM2() {
-		return this.m2;
-	}
-	public int getM3() {
-		return this.m3;
-	}
-	public int getTotal() {
-		return this.total;
-	}
-	public String getCaste() {
-		return this.caste;
-	}
-	public void add(Student s) {
+import java.util.Arrays;
+/**
+ * Class for details.
+ */
+class Details {
+    /**
+     * name.
+     */
+    private String name;
+    /**
+     * dob.
+     */
+    private String dob;
+    /**
+     * s1.
+     */
+    private int subject1;
+    /**
+     * s2.
+     */
+    private int subject2;
+    /**
+     * s3.
+     */
+    private int subject3;
+    /**
+     * total
+     */
+    private int total;
+    /**
+     * category.
+     */
+    private String category;
+    /**
+     * Constructs the object.
+     *
+     * @param      n     { parameter_description }
+     * @param      d     { parameter_description }
+     * @param      s1    The s 1
+     * @param      s2    The s 2
+     * @param      s3    The s 3
+     * @param      t     { parameter_description }
+     * @param      rc    The rectangle
+     */
+    Details(final String n, final String d, final int s1,
+     final int s2, final int s3, final int t, final String rc) {
+        this.name = n;
+        this.dob = d;
+        this.subject1 = s1;
+        this.subject2 = s2;
+        this.subject3 = s3;
+        this.total = t;
+        this.category = rc;
+    }
+    /**
+     * get name.
+     *
+     *Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    String getname() {
+        return this.name;
+    }
+    /**
+     * get dob.
+     *
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     *
+     * @return     { description_of_the_return_value }
+     */
+    String getdob() {
+        return this.dob;
+    }
+    /**
+     * get s1.
+     * Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    int getsubject1() {
+        return this.subject1;
+    }
+    /**
+     * get s2.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    int getsubject2() {
+        return this.subject2;
+    }
+    /**
+     * s3.
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    int getsubject3() {
+        return this.subject3;
+    }
+    /**
+     * total.
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    int gettotal() {
+        return this.total;
+    }
+    /**
+     * cat.
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    String getcategory() {
+        return this.category;
+    }
+    /**
+     * get age.
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    int getage() {
+        int age = 0;
+        final int year = 2018, days = 365, month = 30, ten = 10;
+        String[] token = this.getdob().split("-");
+        age += (year - Integer.parseInt(token[2])) * days;
+        age += (ten - Integer.parseInt(token[1])) * month;
+        age += Integer.parseInt(token[0]);
 
-	}
-	public int compareTo(final Student that) {
-		if (this.getTotal() < that.getTotal()) {
-			return 1;
-		} else if (this.getTotal() > that.getTotal()) {
-			return -1;
-		} else {
-			if (this.getM3() < that.getM3()) {
-				return 1;
-			} else if (this.getM3() > that.getM3()) {
-				return -1;
-			} else {
-				if (this.getM2() < that.getM2()) {
-					return 1;
-				}
-				else if (this.getM2() > that.getM2()) {
-					return -1;
-				} else {
-					if (this.getM1() < that.getM1()) {
-						return 1;
-					}
-					else if (this.getM1() > that.getM1()) {
-						return -1;
-					}
-				}
-			}
-		}
-		return 0;
+        return age;
+    }
+    /**
+     * compare.
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Details that) {
+        if (this.gettotal() > that.gettotal()) {
+            return 1;
+        } else if (this.gettotal() < that.gettotal()) {
+            return -1;
+        } else {
+            if (this.getsubject3() > that.getsubject3()) {
+                return 1;
+            } else if (this.getsubject3() < that.getsubject3()) {
+                return -1;
+            } else {
+                if (this.getsubject2() > that.getsubject2()) {
+                    return 1;
+                } else if (this.getsubject2() < that.getsubject2()) {
+                    return -1;
+                } else {
+                    if (this.getage() < that.getage()) {
+                        return 1;
+                    } else {
+                        return -1;
+                    }
+                }
+            }
+        }
 
-	}
+    }
+    /**
+     * prints.
+     *  Best case: O(1)
+     *  worst case: O(1)
+     *  Average case: O(1)
+     * @return     { description_of_the_return_value }
+     */
+    String print() {
+        return this.getname() + "," + this.gettotal() + ","
+        + this.getcategory();
+    }
 }
-class Sorting {
-	private final int hundred = 100;
-	private Student[] list;
-	private int size;
-	Sorting() {
-		list = new Student[hundred];
-		size = 0;
-	}
-	public void add(final Student s) {
-        list[size++] = s;
+/**
+ * Class for solution.
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        ///function.
     }
-    public void sort() {
-    	for (int i = 0; i < size; i++) {
-    		Student student = list[i];
-    		int j = i -1;
-    		int count = student.compareTo(list[j]);
-    		while (j >= 0 && count == -1) {
-    			list[j + 1] = list[j];
-    			j--;
-    			if (j >= 0) {
-    				count = student.compareTo(list[j]);
-    			}
-    			list[j + 1] = student;
-    		}
-    	}
+    /**
+     * contains.
+     *
+     * @param      arr   The arr
+     * @param      val   The value
+     * Best case: O(1)
+     *  worst case: O(N)
+     *  Average case: O(N)
+     * @return     { description_of_the_return_value }
+     */
+    static boolean contains(final int[] arr, final int val) {
+        for (int n : arr) {
+            if (val == n) {
+                return true;
+            }
+        }
+        return false;
     }
-    public String toString() {
-    	String str = "";
-    	for (int i = 0; i < size; i++) {
-    		str += list[i].getName() +"," + list[i].getTotal() +"," + list[i].getCaste();
-    	}
-    	return str.substring(0, str.length() - 1);
+    /**
+     * sorts.
+     *
+     *  Best case: O(N^2)
+     *  worst case: O(N^2)
+     *  Average case: O(N^2)
+     * @param      students  The students
+     * @param      n         { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    static Details[] selectionsort(final Details[] students, final int n) {
+        for (int  i = 0; i < n; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (students[j].compareTo(students[min]) > -1) {
+                    min = j;
+                }
+            }
+            Details temp = students[i];
+            students[i] = students[min];
+            students[min] = temp;
+        }
+        return students;
     }
-}
-class Solution {
-	Solution () {
-
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Sorting r = new Sorting();
-		int n = sc.nextInt();
-		//System.out.println(n);
-		int v = sc.nextInt();
-		int un = sc.nextInt();
-		int bc = sc.nextInt();
-		int scc = sc.nextInt();
-		int st = sc.nextInt();
-		for (int i = 0; i < n; i++) {
-			//System.out.println(n);
-		//while(sc.hasNext())
-			String line = sc.nextLine();
-			//System.out.println(line);
-			String[] tokens = sc.nextLine().split(",");
-			r.add(new Student(tokens[0], tokens[1], Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]),
-				Integer.parseInt(tokens[4]), Integer.parseInt(tokens[5]), tokens[6]));
-		}
-		r.sort();
-		System.out.println(r);
-		//if ("open".equal(getCaste()) && )
-
-	}
+    /**
+     * main.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        int vacancies = Integer.parseInt(scan.nextLine());
+        int open = Integer.parseInt(scan.nextLine());
+        int bc = Integer.parseInt(scan.nextLine());
+        int sc = Integer.parseInt(scan.nextLine());
+        int st = Integer.parseInt(scan.nextLine());
+        Details[] students = new Details[n];
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split(",");
+            students[i] = new Details(input[0], input[1],
+                 Integer.parseInt(input[2]),
+                Integer.parseInt(input[2 + 1]),
+                Integer.parseInt(input[2 + 2]),
+                Integer.parseInt(input[2 + 2 + 1]), input[2 + 2 + 2]);
+        }
+        students = selectionsort(students, n);
+        for (int i = 0; i < n; i++) {
+            System.out.println(students[i].print());
+        }
+        System.out.println();
+        for (int j = 0; j < open; j++) {
+            System.out.println(students[j].print());
+        }
+        int[] indices = new int[bc + sc + st];
+        int i = 0;
+        for (int k = open; k < n; k++) {
+            if (students[k].getcategory().equals("BC") && bc > 0) {
+                indices[i++] = k;
+                bc--;
+            } else if (students[k].getcategory().
+                equals("SC") && sc > 0) {
+                indices[i++] = k;
+                sc--;
+            } else if (students[k].getcategory().
+                equals("ST") && st > 0) {
+                indices[i++] = k;
+                st--;
+            }
+        }
+        if (bc > 0) {
+            for (int k = open; k < n; k++) {
+            if (students[k].getcategory().equals("Open") && bc > 0) {
+                    if (!contains(indices, k)) {
+                        indices[i++] = k;
+                        bc--;
+                    }
+                }
+            }
+        }
+        if (sc > 0) {
+            for (int k = open; k < n; k++) {
+            if (students[k].getcategory().equals("Open") && sc > 0) {
+                    if (!contains(indices, k)) {
+                        indices[i++] = k;
+                        sc--;
+                    }
+                }
+            }
+        }
+        if (st > 0) {
+            for (int k = open; k < n; k++) {
+            if (students[k].getcategory().equals("Open") && st > 0) {
+                    if (!contains(indices, k)) {
+                        indices[i++] = k;
+                        st--;
+                    }
+                }
+            }
+        }
+        Arrays.sort(indices);
+        for (int k = 0; k < indices.length; k++) {
+            System.out.println(students[indices[k]].print());
+        }
+    }
 }
