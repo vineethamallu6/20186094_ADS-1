a113566106b687d1d67311ba6c7eb54f4e2d1033
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-27 11:36:16 -0700

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ Assignments/m24/1/Solution.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+/**
+ * Class for solution.
+ */
+class Student {
+	private String name;
+	private double marks;
+	Student(final String n, final double m) {
+		this.name = n;
+		this.marks = m;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+	public double getMarks() {
+		return this.marks;
+	}
+}
+public final class Solution {
+    /**
+     * client class.
+     */
+    private Solution() {
+    }
+    /**
+     * main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = Integer.parseInt(scan.nextLine());
+        SeparateChainingHashST<Integer, Student> students= new
+        SeparateChainingHashST<>();
+        Student stu = null;
+        // LinearProbingHashST<String, Integer> hash
+        // = new LinearProbingHashST<String, Integer>();
+        //BinarySearchTree bst = new BinarySearchTree();
+        for (int i = 0; i < n; i++) {
+        	String[] token = scan.nextLine().split(",");
+        	stu = new Student(token[1], Double.parseDouble(token[2]));
+        }
+        int m = Integer.parseInt(scan.nextLine());
+        for (int i = 0; i < m; i++) {
+        	String[] check = scan.nextLine().split(" ");
+        	if (check[2].equals("1")) {
+                int key = Integer.parseInt(check[1]);
+                if (students.contains(key)) {
+                    System.out.println(students.get(key).getName());
+                }
+            }
+        }
+}
+}
