9ac4ee895fdfb4f03f0f02b5e661f72e2776d0fb
vineethamallu6 <vineethamallu6@msitprogram.net>
2018-10-27 11:57:19 -0700

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- Assignments/m24/1/Solution.java
+++ Assignments/m24/1/Solution.java
@@ -4,40 +4,40 @@ import java.util.Scanner;
  */
 class Student {
     /**
-     * { var_description }
+     * name.
      */
-	private String name;
+    private String name;
     /**
-     * { var_description }
+     * marks.
      */
-	private double marks;
+    private double marks;
     /**
      * Constructs the object.
      *
      * @param      n     { parameter_description }
      * @param      m     { parameter_description }
      */
-	Student(final String n, final double m) {
-		this.name = n;
-		this.marks = m;
-	}
+    Student(final String n, final double m) {
+        this.name = n;
+        this.marks = m;
+    }
     /**
      * Gets the name.
      *
      * @return     The name.
      */
 
-	public String getName() {
-		return this.name;
-	}
+    public String getName() {
+        return this.name;
+    }
     /**
      * Gets the marks.
      *
      * @return     The marks.
      */
-	public double getMarks() {
-		return this.marks;
-	}
+    public double getMarks() {
+        return this.marks;
+    }
 }
 public final class Solution {
     /**
@@ -53,18 +53,18 @@ public final class Solution {
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = Integer.parseInt(scan.nextLine());
-        SeparateChainingHashST<Integer, Student> students= new
+        SeparateChainingHashST<Integer, Student> students = new
         SeparateChainingHashST<>();
         Student stu = null;
         for (int i = 0; i < n; i++) {
-        	String[] token = scan.nextLine().split(",");
-        	stu = new Student(token[1], Double.parseDouble(token[2]));
+            String[] token = scan.nextLine().split(",");
+            stu = new Student(token[1], Double.parseDouble(token[2]));
             students.put(Integer.parseInt(token[0]), stu);
         }
         int m = Integer.parseInt(scan.nextLine());
         for (int i = 0; i < m; i++) {
-        	String[] check = scan.nextLine().split(" ");
-        	if (check[2].equals("1")) {
+            String[] check = scan.nextLine().split(" ");
+            if (check[2].equals("1")) {
                 int roll = Integer.parseInt(check[1]);
                 if (students.contains(roll)) {
                     System.out.println(students.get(roll).getName());
@@ -79,7 +79,7 @@ public final class Solution {
                     System.out.println("Student doesn't exists...");
                 }
             }
-
-            }
         }
     }
+}
+
